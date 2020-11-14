2ff8cd223305c012362eb124cfbc6e5d45255d49
pinwei wu <pw.amy.wu@gmail.com>
2020-11-13 19:16:16 -0600

    update readme and before coding page

--- src/components/Before.js
+++ src/components/Before.js
@@ -1,98 +1,74 @@
 import React from 'react';
-import project1 from '../images/LearningHowToLearn.JPG';
-import project2 from '../images/FromPortlandToPortland.JPG';
-import project3 from '../images/AroundUs.JPG';
-import project4 from '../images/baseCalculator.JPG';
+import statics from '../images/statistics2.jpg';
+import engineer from '../images/engineer.jpg';
+import physics from '../images/probes.jpg';
 export default function Before() {
     return (
         <main className="content">
-            <section className="projects">
-                <h2 className="projects__text lobster">Before Coding</h2>
-                <ul className="projects__box">
-                    <li className="projects__item">
-                        <a
-                            href="https://iceicery.github.io/web_project_1/"
-                            target="_blank"
-                            rel="noreferrer"
-                        ><img src={project1} className="projects__img" alt="project"
-                            /></a>
-                        <h3 className="projects__title">Informitive website</h3>
-                        <ul className="projects__tech">
-                            <li className="projects__tech-item">Html</li>
-                            <li className="projects__tech-item">CSS</li>
-                            <li className="projects__tech-item">JavaScript</li>
-                            <a
-                                href="https://github.com/iceicery/web_project_1"
+            <section className="before">
+                <h2 className="before__text lobster">Before Coding</h2>
+                <ul className="before__box">
+                    <li className="before__item">
+                        <img src={statics} className="before__item-img" alt="project" />
+                        <div className="before__item-textbox">
+                            <h3 className="before__item-title lobster">I was a statistical analyst.</h3>
+                            <p>I worked at <a
+                                href="https://hmc.pennstatehealth.org/"
                                 target="_blank"
                                 rel="noreferrer"
-                            >
-                                <li className="projects__tech-item projects__tech-link">Code</li>
-                            </a>
-                        </ul>
+                                className="before__item-link"
+                            >Milton S. Hershey Medical Center</a> as a research intern.I compared the market share of melanoma cancer for vaious hospital. I developed data
+                            cleaning code, built formatted data sets and developed R program to calculate age
+                            adjusted cancer crude rate and rate ratio. I provided data and helpd build <a
+                                    href="https://psulss.maps.arcgis.com/apps/Cascade/index.html?appid=0fed8fee47cd483b963f056a774b0467"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="before__item-link">
+                                    story map on esri.com</a> regarding cancer in catchment area, PA.</p>
+                        </div>
+
                     </li>
-                    <li className="projects__item">
-                        <a
-                            href="https://iceicery.github.io/web_project_3/"
-                            target="_blank"
-                            rel="noreferrer"
-                        ><img src={project2} className="projects__img" alt="project"
-                            /></a>
-                        <h3 className="projects__title">Informitive website</h3>
-                        <ul className="projects__tech">
-                            <li className="projects__tech-item">Html</li>
-                            <li className="projects__tech-item">CSS</li>
-                            <li className="projects__tech-item">JavaScript</li>
-                            <a
-                                href="https://github.com/iceicery/web_project_3"
+                    <li className="before__item">
+                        <img src={engineer} className="before__item-img" alt="project" />
+                        <div className="before__item-textbox">
+                            <h3 className="before__item-title lobster">I was an engineer.</h3>
+                            <p>I worked at <a
+                                href="https://auo.com/en-global"
                                 target="_blank"
                                 rel="noreferrer"
-                            >
-                                <li className="projects__tech-item projects__tech-link">Code</li>
-                            </a>
-                        </ul>
+                                className="before__item-link"
+                            >AU Optronic Corporation</a> as a TFT LCD Panel design engineer.I developed panels based on customers’ requests, monitored panels’ performance and solved problems.
+                             I worked with the design team and factories. I co-wrote a patent-<a
+                                    href="https://patents.google.com/patent/US20110310032"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className="before__item-link"
+                                > Pixel array and display panel having the same.</a>
+                            </p>
+                        </div>
+
                     </li>
-                    <li className="projects__item">
-                        <a
-                            href="https://iceicery.github.io/web_project_4/dist/index.html"
-                            target="_blank"
-                            rel="noreferrer"
-                        ><img src={project3} className="projects__img" alt="project"
-                            /></a>
-                        <h3 className="projects__title">Informitive website</h3>
-                        <ul className="projects__tech">
-                            <li className="projects__tech-item">Html</li>
-                            <li className="projects__tech-item">CSS</li>
-                            <li className="projects__tech-item">JavaScript</li>
-                            <a
-                                href="https://github.com/iceicery/web_project_4"
+                    <li className="before__item">
+                        <img src={physics} className="before__item-img" alt="project" />
+                        <div className="before__item-textbox">
+                            <h3 className="before__item-title lobster">I was a physics lover.</h3>
+                            <p>I was a research assistant of applied physics at <a
+                                href="http://nthu-en.site.nthu.edu.tw/"
                                 target="_blank"
                                 rel="noreferrer"
-                            >
-                                <li className="projects__tech-item projects__tech-link">Code</li>
-                            </a>
-                        </ul>
-                    </li>
-                    <li className="projects__item">
-                        <a
-                            href="https://iceicery.github.io/BaseCaculator/"
-                            target="_blank"
-                            rel="noreferrer"
-                        ><img src={project4} className="projects__img" alt="project"
-                            /></a>
-                        <h3 className="projects__title">Informitive website</h3>
-                        <ul className="projects__tech">
-                            <li className="projects__tech-item">Html</li>
-                            <li className="projects__tech-item">CSS</li>
-                            <li className="projects__tech-item">JavaScript</li>
-                            <a
-                                href="https://github.com/iceicery/BaseCaculator"
+                                className="before__item-link"
+                            >National Tsing Hua University</a> I simulated the best shape for the <a
+                                href="https://patents.google.com/patent/US20070268016A1/en"
                                 target="_blank"
                                 rel="noreferrer"
-                            >
-                                <li className="projects__tech-item projects__tech-link">Code</li>
-                            </a>
-                        </ul>
+                                className="before__item-link"
+                            >SSTM</a> probe based on fundamental electromagnetism and finite element analysis.
+                        I also produced the probe using chemical etching and focus iron Bean.  I wrote a M.S. thesis "Pixel array and display panel having the same".
+                            </p>
+                        </div>
+
                     </li>
+
                 </ul>
             </section>
         </main>
